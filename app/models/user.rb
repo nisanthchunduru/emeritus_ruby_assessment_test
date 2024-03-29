dad01d7ecabdcb86345a11ebe9dd2abd759caf1e
Nisanth Chunduru <nisanth074@gmail.com>
2024-03-29 12:12:56 +0530

    Add a User#favourite_teachers method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  # enum kind: { student: 0, teacher: 1, student_teacher: 2 }
+  has_many :enrollments
+  has_many :teachers, through: :enrollments
+  has_many :favorite_teachers, -> { where(enrollments: { favorite: true }) }, through: :enrollments, source: :teacher
 end
