21a0a14c419981095fda230b837c7564311b6e33
Nisanth Chunduru <nisanth074@gmail.com>
2024-03-29 12:18:52 +0530

    Add a User#classmates method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  # enum kind: { student: 0, teacher: 1, student_teacher: 2 }
   has_many :enrollments
+  has_many :programs, through: :enrollments
   has_many :teachers, through: :enrollments
   has_many :favorite_teachers, -> { where(enrollments: { favorite: true }) }, through: :enrollments, source: :teacher
+
+  enum kind: { student: 0, teacher: 1, student_teacher: 2 }
+
+  def classmates
+    User
+      .joins(:programs)
+      .where(programs: programs)
+      .where.not(id: self.id)
+  end
 end
